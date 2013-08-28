2999c32e1cfdf335c1473817d3b4a31b54b943c8
Matt Hoover <mhoover@jp.ncsasports.org>
2013-08-28 12:44:23 -0500

    removed commented driver code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,28 +9,3 @@ computer = ComputerPlayer.new(board)
 game = GameController.new(board, interface, computer)
 
 game.play
-
-# puts interface.clear_screen
-# puts board
-
-# until board.won? || board.tied?
-# 	move = 0
-# 	until interface.input_valid?(move) && board.tile_open?(move)
-# 		puts interface.prompt_human
-# 		move = interface.get_input
-# 	end
-# 	board.update(move, "X")
-
-# 	puts interface.clear_screen
-# 	puts board
-
-# 	break if board.won? || board.tied?
-
-# 	sleep(0.1)
-# 	board.update(computer.random_move, "O")
-
-# 	puts interface.clear_screen
-# 	puts board
-# end
-
-# puts interface.display_results(board)
